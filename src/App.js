fe52b858f6b5816408592200c3a16e38bc52b98b
melissaair <100801990+melissaair@users.noreply.github.com>
2022-08-14 00:39:40 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Weather />
-      <div className="Github">
-        <p><a href="https://github.com/melissaair/weather-react">Open-source code</a>, by <a href="https://github.com/melissaair">Melissa Ibarra-Ramos</a></p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<Weather />
+				<footer>
+					<p>
+						<a href="https://github.com/melissaair/weather-react">
+							Open-source code
+						</a>
+						, by{" "}
+						<a href="https://github.com/melissaair">Melissa Ibarra-Ramos</a>
+					</p>
+				</footer>
+			</div>
+		</div>
+	);
 }
